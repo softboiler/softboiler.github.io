9d1aab58ca9a365af0e11f9d1536e3455b67a74d
blakeNaccarato <blake.naccarato@gmail.com>
2024-07-03 13:09:39 -0700

    Bump version

--- scripts/softboiler_github_io_tools/sync.py
+++ scripts/softboiler_github_io_tools/sync.py
@@ -12,7 +12,7 @@ from subprocess import run
 
 from softboiler_github_io_tools.types import Dep, PythonVersion, SubmoduleInfoKind, ops
 
-MINIMUM_PYTHON = "3.11"
+MINIMUM_PYTHON = "3.10"
 """This project's default Python version."""
 
 # ! Dependencies
