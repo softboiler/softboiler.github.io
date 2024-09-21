4a34d7b6ab60e2ebccfd81e9ce06529893dfff7e
blakeNaccarato <blake.naccarato@gmail.com>
2024-09-21 12:50:13 -0700

    Sync with template

--- docs/index.md
+++ docs/index.md
@@ -1,15 +1,16 @@
 # softboiler_github_io
 
-[![All Contributors](https://img.shields.io/github/all-contributors/softboiler/softboiler.github.io?color=ee8449&style=flat-square)](../README.md#contributors)
+[![All Contributors](https://img.shields.io/github/all-contributors/softboiler/softboiler.github.io?color=ee8449&style=flat-square)](contributors)
 
 My dissertation projects facilitate the study of nucleate pool boiling with a focus on bubble visualization and analysis:
 
-- [`boilercv`](https://softboiler.org/boilercv): Computer vision routines.
-- [`boilerdata`](https://softboiler.org/boilerdata): Data processing pipeline.
-- [`boilerdaq`](https://softboiler.org/boilerdaq): Data acquisition for the experimental apparatus.
-- [`boilercore`](https://softboiler.org/boilercore): Utility and test code common to the above. Modules may be split into independent libraries, and may be generally useful for other research. For instance, `captivate` facilitates video and image viewing.
-- [`boilercine`](https://softboiler.org/boilercine): Read and process CINE files for the [`boilercv`](https://github.com/softboiler/boilercv) pipeline. Will probably migrate to [`pims`](https://pypi.org/project/PIMS/), time permitting.
-- [`copier-python`](https://github.com/blakeNaccarato/copier-python): General-purpose Python project template. I intend to furnish a science-focused version of this template in `softboiler`.
+- [`boilercv`](https://github.com/softboiler/boilercv): Computer vision routines and data processing pipeline. DVC pipeline and equation generation.
+- [`copier-pipeline`](https://github.com/softboiler/copier-pipeline): Project template for engineering research data pipelines.
+- [`boilerdata`](https://github.com/softboiler/boilerdata): Thermal data processing pipeline.
+- [`context_models`](https://github.com/softboiler/context_models): Core functionality for DVC pipeline generation.
+- [`boilerdaq`](https://github.com/softboiler/boilerdaq): Data acquisition for the experimental apparatus.
+- [`boilercore`](https://github.com/softboiler/boilercore): Utility and test code common to the above. Modules may be split into independent libraries, and may be generally useful for other research. For instance, `captivate` facilitates video and image viewing.
+- [`boilercine`](https://github.com/softboiler/boilercine): Read and process CINE files for the [`boilercv`](https://github.com/softboiler/boilercv) pipeline. Will probably migrate to [`pims`](https://pypi.org/project/PIMS/), time permitting.
 
 Another goal of this organization's [website](https://softboiler.org/) will be to aggregate and highlight other thermal science research software in the field, with a focus on open-source projects that follow the FAIR Guiding Principles. Such projects focus on improving the **F**indability, **A**ccessibility, **I**nteroperability, and **R**euse of their data, methods, and research.
 
--- scripts/Initialize-LinuxMacOS.sh
+++ scripts/Initialize-LinuxMacOS.sh
@@ -1,10 +1,10 @@
-# Initialize Linux or MacOS machines
+# ? Initialize Linux or MacOS machines
 
 BREW="brew"
 
-# Initialize MacOS machine
+# ? Initialize MacOS machine
 if [[ $OSTYPE == "darwin"* ]]; then
-    # Install brew if missing
+    # ? Install brew if missing
     if ! command -v $BREW &> /dev/null; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         if [ -x "/usr/local/bin/brew" ]; then
@@ -14,25 +14,21 @@ if [[ $OSTYPE == "darwin"* ]]; then
         fi
     fi
     $BREW update
-    # Install Git
+    # ? Install Git
     $BREW install git
-    # Install PowerShell
+    # ? Install PowerShell
     $BREW install powershell/tap/powershell
-    # Install appropriate Python (can't easily use `python-build-standalone`)
-    $BREW install python@3.10
-    # Install Python Launcher
-    $BREW install python-launcher
-    # Install GitHub API
+    # ? Install GitHub API
     $BREW install gh
     return
 fi
 
-# Initialize Linux machines
+# ? Initialize Linux machines
 
 sudo -v
 SNAP="snap"
 
-# Handle WSL-specific systemd issue
+# ? Handle WSL-specific systemd issue
 if grep -i microsoft /proc/version > /dev/null && [ ! -f /etc/wsl.conf ]; then
     sudo tee -a /etc/wsl.conf > /dev/null << EOF
 [boot]
@@ -42,48 +38,36 @@ EOF
     return
 fi
 
-# Install snap and Python Launcher as needed on Ubuntu or Fedora
+# ? Install snap as needed on Ubuntu or Fedora
 if [ "$(command -v apt)" ]; then
     PKG="apt"
     sudo apt update
-    # Install snap if missing
+    # ? Install snap if missing
     if ! command -v $SNAP &> /dev/null; then
         sudo $PKG install -y snapd
         SNAP="/usr/bin/snap"
-    fi
-    # Install Python launcher in Ubuntu-specific way if missing
-    if ! command -v py &> /dev/null; then
-        # Install Linuxbrew/Homebrew if missing
-        if ! command -v $BREW &> /dev/null; then
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-            test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-            test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-        fi
-        if grep -i microsoft /proc/version > /dev/null; then
-            # Linuxbrew/Homebrew update hangs in WSL
-            export HOMEBREW_NO_AUTO_UPDATE=1
-        else
-            brew update
-        fi
-        brew install python-launcher
     fi
 elif [ "$(command -v dnf)" ]; then
     PKG="dnf"
     sudo dnf makecache
-    # Install snap if missing
+    # ? Install snap if missing
     if ! command -v $SNAP &> /dev/null; then
         sudo $PKG install -y snapd
         SNAP="/usr/bin/snap"
-        # Perform Fedora-specific snap setup
+        # ? Perform Fedora-specific snap setup
         sudo systemctl enable --now snapd.socket
         sudo ln -s /var/lib/snapd/snap /snap
     fi
-    # Install Python launcher in Fedora-specific way
-    sudo $PKG install python-launcher
 else return
 fi
 
-# Install Git, PowerShell, VSCode, and GitHub API on Linux
+# ? Install Git, PowerShell, VSCode, and GitHub API on Linux
 sudo $PKG install -y git gh
 sudo $SNAP install powershell --classic
 sudo $SNAP install code --classic
+
+# ? Install `uv`
+curl --proto '=https' --tlsv1.2 -LsSf 'https://astral.sh/uv/install.sh' | sh
+
+# ? Finish machine setup
+pwsh -Command '. ./dev.ps1 && Initialize-Machine'
